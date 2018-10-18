0279d0bb7ff6ed3d413744f855f1cf3667538b3a
Tom Rowson <tom.rowson@gmail.com>
2018-10-18 14:52:46 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,13 @@
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
+def prime?(num)
+  if num <= 1 
+    return false
+  else array = (2..(num-1)).to_a
+  new_array = []
+    array.each { |divider| new_array << num % divider }
+  if new_array.include?(0)
+  return false
+  else
+  return true
+  end
+end
 end
